e91b61252283302cb5b342799281579e341c68b8
en-technology4 <131554693+en-technology4@users.noreply.github.com>
2024-05-18 14:12:35 +0330

    Update subService.go

--- sub/subService.go
+++ sub/subService.go
@@ -355,7 +355,7 @@ func (s *SubService) genVlessLink(inbound *model.Inbound, email string) string {
 		if len(ws["host"].(string)) > 0 {
 			params["host"] = ws["host"].(string)
 		} else {
-			params["host"] = s.address.(string)
+			params["host"] = address
 		}
 		params["host"] = ws["host"].(string)
 		headers, _ := ws["headers"].(map[string]interface{})
@@ -409,7 +409,7 @@ func (s *SubService) genVlessLink(inbound *model.Inbound, email string) string {
 		if sniValue, ok := searchKey(tlsSetting, "serverName"); ok {
 			params["sni"], _ = sniValue.(string)
 		} else {
-			params["sni"], _ = s.address.(string)
+			params["sni"], _ = address
 		}
 
 		tlsSettings, _ := searchKey(tlsSetting, "settings")
